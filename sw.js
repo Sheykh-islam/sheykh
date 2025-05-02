44ee2d4fe84dad2572093735c1cb05394f5bf611
Sheykh-islam <spanchpm@gmail.com>
2025-05-02 22:59:24 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'my-cards-v1';
+const urlsToCache = [
+  '/',
+  '/index.html', // Или ваш основной HTML-файл
+  // Пропишите сюда все важные CSS/JS/картинки, которые хотите кэшировать:
+  'https://static.tildacdn.info/css/tilda-grid-3.0.min.css',
+  'https://static.tildacdn.info/img/tildafavicon.ico',
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
+});
